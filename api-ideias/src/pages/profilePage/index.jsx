6fb2099c3b76a7aa436caf0d3d6ee7667e02e6b8
Jonas Leite <jonas.programador01@gmail.com>
2024-05-09 01:40:23 -0300

    feat: impleting the logic for the ideas not aproved

--- api-ideias/src/pages/profilePage/index.jsx
+++ api-ideias/src/pages/profilePage/index.jsx
@@ -10,21 +10,35 @@ import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 
 const ProfilePage = () => {
   const [countIdeas, setCount] = useState(0);
+  const [ideaCardUrl, setIdeaCardUrl] = useState('show-my?limit=1&offset=0');
+  const [title, setTitle] = useState('Suas ideias de projetos');
+  const [buttonText, setButtonText] = useState('Ideias a serem Aprovadas');
+  const [reload, setReload] = useState(false); // Adicione este estado
   const headers = useAuthHeader();
   const Token = headers ? headers.replace("x-acess-token", ""): null;
   const authUser = useAuthUser();
   const getData = async()=>{
-    const data = await FetchApi("GET", `https://banco-de-ideiasapi.up.railway.app/project/show-my?limit=0`, '', Token)
+    const data = await FetchApi("GET", `https://banco-de-ideiasapi.up.railway.app/project/${ideaCardUrl}`, '', Token)
     console.log(data.totalOfProjects)
     setCount(data.totalOfProjects)
-    
   }
   useEffect(()=>{
-   
     getData()
-
-  },[])
+  },[ideaCardUrl, reload]) 
  
+  const handleButtonClick = () => {
+    if (ideaCardUrl === 'show-my?limit=1&offset=0') {
+      setIdeaCardUrl('/show-standby?limit=5&offset=0');
+      setButtonText('Ideias Aprovadas');
+      setTitle('Ideias a serem Aprovadas');
+    } else {
+      setIdeaCardUrl('show-my?limit=1&offset=0');
+      setButtonText('Ideias a serem Aprovadas');
+      setTitle('Suas ideias de projetos');
+    }
+    setReload(!reload); 
+  }
+
   return (
     <div>
       <Header to1="/" link1="Home" to2="/ideias" link2="Todas ideias"/>
@@ -36,16 +50,17 @@ const ProfilePage = () => {
               <h2 className="subtitle">{authUser.name}</h2>
 
               <Link className="btn" to="/criar">Criar ideia</Link>
-              <Link className="btn">Ideias a serem Aprovadas</Link>
+              <button className="btn" onClick={handleButtonClick}>{buttonText}</button>
             </div>
           </div>
           <div className="profile-ideas">
             <div className="ideas">
-              <span className="subtitle idea">Suas ideias de projetos</span>
+            <span className="subtitle idea">{title}</span> 
               <IdeaCard
-                url={ `show-my?limit=1&offset=0`}
+                url={ideaCardUrl}
                 editable={true}
                 cards={2}
+                key={ideaCardUrl}
               />
             </div>
           </div>
