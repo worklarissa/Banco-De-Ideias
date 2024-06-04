c0215daa40b53a95610579e67bb4e9dc1f2e8138
Jonas Leite <jonasprogramador00@gmail.com>
2024-06-04 19:38:57 -0300

    fix:fixing the bug in the searchbar

--- front-end/src/pages/pageIdea/index.jsx
+++ front-end/src/pages/pageIdea/index.jsx
@@ -19,7 +19,7 @@ export const Ideas = () => {
   }
  
   const url = searchTerm 
-  ? `show-searched?limit=6&offset=0&title=${searchTerm}`
+  ? `show-searched?limit=10&offset=0&title=${searchTerm}`
   : "show-valid?limit=6&offset=0";
 
 
@@ -55,7 +55,7 @@ export const Ideas = () => {
       <IdeaCard 
       cards={4}  
       url={url} 
-      key={key} 
+      key={url} 
       />
     
     </div>
