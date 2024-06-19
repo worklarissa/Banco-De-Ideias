b755b5ceffa383bf42cfc2bdc773681648207cf4
Anderson <loonander@gmail.com>
2024-06-19 18:01:32 -0300

    removing search bar sugestions for now

--- front-end/src/components/searchBar/index.jsx
+++ front-end/src/components/searchBar/index.jsx
@@ -6,35 +6,35 @@ const SearchBar = ({ onSearchTermChange,onClickSearch }) => {
   const [searchTerm,setSearchTerm] = useState('')
   const [suggestions, setSuggestions] = useState([]);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    const fetchSuggestions = async () => {
-      if (searchTerm.length > 1) {
-        try {
-          const response = await fetch(
-            `https://api.datamuse.com/sug?s=${searchTerm}`
-          );
-          const data = await response.json();
+  //   const fetchSuggestions = async () => {
+  //     if (searchTerm.length > 1) {
+  //       try {
+  //         const response = await fetch(
+  //           `https://api.datamuse.com/sug?s=${searchTerm}`
+  //         );
+  //         const data = await response.json();
        
-          const wordSuggestions = data.map((suggestion) => suggestion.word);
-          setSuggestions(wordSuggestions);
-        } catch (error) {
-          console.error("Erro ao buscar sugestões:", error);
-        }
-      } else {
-        setSuggestions([]);
-      }
-    };
+  //         const wordSuggestions = data.map((suggestion) => suggestion.word);
+  //         setSuggestions(wordSuggestions);
+  //       } catch (error) {
+  //         console.error("Erro ao buscar sugestões:", error);
+  //       }
+  //     } else {
+  //       setSuggestions([]);
+  //     }
+  //   };
 
-    const timeoutId = setTimeout(() => {
-      fetchSuggestions();
-    }, 300);
+  //   const timeoutId = setTimeout(() => {
+  //     fetchSuggestions();
+  //   }, 300);
 
 
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [searchTerm]);
+  //   return () => {
+  //     clearTimeout(timeoutId);
+  //   };
+  // }, [searchTerm]);
 
   const handleSuggestionClick = (suggestion) => {
     setSearchTerm(suggestion);
