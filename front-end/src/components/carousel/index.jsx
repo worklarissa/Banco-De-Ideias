88e78485c8384b1a9f230d16cde63e4d10ffb4e3
JonasLProgramador <jonas.programador01@gmail.com>
2024-07-17 04:48:28 -0300

    feat: implementing the responsive for cards and add the dinamic card

--- front-end/src/components/carousel/index.jsx
+++ front-end/src/components/carousel/index.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
 import { FetchApi } from "../../utils/Fetch";
-import "./carousel.css"
+import "./carousel.css";
 import Loading from "../loader";
 
 function IdeaCarousel({ url }) {
   const [posts, setPosts] = useState([]);
   const [allPosts, setAllPosts] = useState([]);
-  const [removeLoading, setRemoveLoading] = useState(false)
+  const [removeLoading, setRemoveLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [itemsPerSlide, setItemsPerSlide] = useState(null);
 
   const loadPosts = () => {
     FetchApi("GET", `${url}?page=${page}`, "", null).then((data) => {
@@ -17,7 +18,7 @@ function IdeaCarousel({ url }) {
         setPosts(data.projects);
         setAllPosts((prevPosts) => [...prevPosts, ...data.projects]);
         setPage((prevPage) => prevPage + 1);
-        setRemoveLoading(true)
+        setRemoveLoading(true);
       } else {
         console.error("Data is not an array:", data);
       }
@@ -26,6 +27,19 @@ function IdeaCarousel({ url }) {
 
   useEffect(() => {
     loadPosts();
+  }, []);
+
+  useEffect(() => {
+    const updateItemsPerWidth = () => {
+      const width = window.innerWidth;
+      const items = width < 320 ? 1 : width <= 540 ? 2  :  width  <= 768 ? 3 : width  >= 992 ?  4 : 3 ;
+      setItemsPerSlide(items);
+    };
+
+    window.addEventListener("resize", updateItemsPerWidth);
+    updateItemsPerWidth();
+    console.log(itemsPerSlide);
+    return () => window.removeEventListener("resize", updateItemsPerWidth);
   }, []);
 
   const handleSelect = (selectedIndex, e) => {
@@ -39,9 +53,8 @@ function IdeaCarousel({ url }) {
     }
   };
 
-  
   const groupedPosts = posts.reduce((grouped, post, index) => {
-    const groupIndex = Math.floor(index / 3);
+    const groupIndex = Math.floor(index / itemsPerSlide);
 
     if (!grouped[groupIndex]) {
       grouped[groupIndex] = [];
@@ -56,28 +69,26 @@ function IdeaCarousel({ url }) {
     <>
       {!removeLoading && <Loading />}
       <Carousel onSelect={handleSelect}>
-    
-    {groupedPosts.map((group, idx) => (
-      <Carousel.Item key={idx}>
-        <div className=" carousel d-flex justify-content-around">
-          {group.map((post, idx) => (
-            <div
-              key={idx}
-              style={{ backgroundColor: "#" + post.postColor}}
-              className="card-carousel"
-            >
-              <div style={{ backgroundColor:  post.postColor, flex: 1 }}>
-                <Card.Title>{post.title}</Card.Title>
-                <Card.Text>{post.text}</Card.Text>
-              </div>
+        {groupedPosts.map((group, idx) => (
+          <Carousel.Item key={idx}>
+            <div className=" carousel d-flex justify-content-around">
+              {group.map((post, idx) => (
+                <div
+                  key={idx}
+                  style={{ backgroundColor: "#" + post.postColor }}
+                  className="card-carousel"
+                >
+                  <div style={{ backgroundColor: post.postColor, flex: 1 }}>
+                    <Card.Title>{post.title}</Card.Title>
+                    <Card.Text>{post.text}</Card.Text>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </Carousel.Item>
-    ))}
-  </Carousel>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </>
-   
   );
 }
 
